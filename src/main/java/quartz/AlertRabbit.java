f71590e11aefa2e88b756437e2490d4e53b49d74
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-04-29 21:31:01 +0300

    1.1. Job c параметрами [#296939]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -2,6 +2,8 @@ package quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
@@ -13,40 +15,42 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public class AlertRabbit {
     public static void main(String[] args) {
         Properties properties = new Properties();
-        try {
-        InputStream io = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties");
-        properties.load(io);
-        if (io != null) {
-            io.close();
-        }
-        Class.forName(properties.getProperty("driver-class-name"));
-        Connection connection = DriverManager.getConnection(
-                properties.getProperty("url"),
-                properties.getProperty("username"),
-                properties.getProperty("password")
-        );
-            PreparedStatement statement = connection.prepareStatement(
-                    "CREATE TABLE IF NOT EXISTS rabbit (id serial primary key, created_date timestamp);"
-            );
-            statement.executeUpdate();
-            statement.execute();
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDataMap data = new JobDataMap();
-            data.put("connection", connection);
-            JobDetail job = newJob(Rabbit.class)
-                    .usingJobData(data)
-                    .build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(properties.getProperty("rabbit.interval")))
-                    .repeatForever();
-            SimpleTrigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-            Thread.sleep(10000);
-            scheduler.shutdown();
+            try (InputStream io = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+                properties.load(io);
+                if (io != null) {
+                    io.close();
+                }
+                Class.forName(properties.getProperty("driver-class-name"));
+                try (Connection connection = DriverManager.getConnection(
+                        properties.getProperty("url"),
+                        properties.getProperty("username"),
+                        properties.getProperty("password")
+                )) {
+                    try (PreparedStatement statement = connection.prepareStatement(
+                            "CREATE TABLE IF NOT EXISTS rabbit (id serial primary key, created_date timestamp);"
+                    )) {
+                        statement.executeUpdate();
+                        statement.execute();
+                        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                        scheduler.start();
+                        JobDataMap data = new JobDataMap();
+                        data.put("connection", connection);
+                        JobDetail job = newJob(Rabbit.class)
+                                .usingJobData(data)
+                                .build();
+                        SimpleScheduleBuilder times = simpleSchedule()
+                                .withIntervalInSeconds(Integer.parseInt(properties.getProperty("rabbit.interval")))
+                                .repeatForever();
+                        SimpleTrigger trigger = newTrigger()
+                                .startNow()
+                                .withSchedule(times)
+                                .build();
+                        scheduler.scheduleJob(job, trigger);
+                        Thread.sleep(10000);
+                        scheduler.shutdown();
+                    }
+
+                }
         } catch (Exception e) {
             e.printStackTrace();
         }
