037edfdf04c6f1d8a531db94e1289cd25bfdf0a7
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-05-10 11:20:07 +0300

    4. Схема таблицы Post [#296948]

--- src/main/java/grabber/Post.java
+++ src/main/java/grabber/Post.java
@@ -1,4 +1,53 @@
 package grabber;
 
+import java.time.LocalDateTime;
+
 public class Post {
+
+    private String text;
+    private String link;
+    private String heading;
+
+    public Post() {
+
+    }
+
+    public Post(String heading, String link, String text) {
+        this.text = text;
+        this.link = link;
+        this.heading = heading;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "text='" + text + '\'' +
+                ", link='" + link + '\'' +
+                ", heading='" + heading + '\'' +
+                '}';
+    }
 }
