41af5fddc545958c7a927a8b9d7d0081d76f1f01
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-05-19 00:35:24 +0300

    7. Web. [#296947]

--- src/main/java/grabber/Crabber.java
+++ src/main/java/grabber/Crabber.java
@@ -6,6 +6,9 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.Properties;
 
@@ -71,11 +74,35 @@ public class Crabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer
+                    .parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        ).start();
+    }
+
     public static void main(String[] args) throws IOException, SchedulerException {
         Crabber crabber = new Crabber();
         crabber.cfg();
         Scheduler scheduler = crabber.scheduler();
         Store store = crabber.store();
         crabber.init(new SqlRuParse(), store, scheduler);
+        crabber.web(store);
     }
 }
