b9d89d1b8c9d7b552f4abd113bcf486d548da98c
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-06-25 22:27:59 +0300

    3. Шаблонизатор. [#855]

---
+++ src/main/java/template/Generator.java
@@ -0,0 +1,8 @@
+package template;
+
+import java.util.Map;
+
+public interface Generator {
+
+    String produce(String template, Map<String, String> args);
+}
---
+++ src/test/java/template/GeneratorTest.java
@@ -0,0 +1,43 @@
+package template;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+class GeneratorTest {
+
+    @Test
+    public void whenGenerate() {
+        GenDemo demo = new GenDemo();
+        String keys = "I am a ${name}, who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Mikhail");
+        map.put("subject", "you");
+        String result = "I am a Mikhail, who are you";
+        String rsl = demo.produce(keys, map);
+        assertThat(rsl, is(result));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNoKeys() {
+        GenDemo demo = new GenDemo();
+        String keys = "I am a ${name}, who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("subject", "you");
+        String result = demo.produce(keys, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNotParam() {
+        GenDemo demo = new GenDemo();
+        String keys = "I am a ${name}, who are ${subject}";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Mikhail");
+        map.put("surname", "you");
+        String result = demo.produce(keys, map);
+    }
+}
